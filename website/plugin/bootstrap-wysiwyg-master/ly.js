7675ffa921c209e1e2e78d4b7ec91be885293746
ly <yangliu_akuacom@163.com>
2015-05-24 03:29:23 +0800

    Revert "LMS website"
    This reverts commit 26ad6647bc71eea9876758dd09a6a408b8fc67e9.

--- website/plugin/bootstrap-wysiwyg-master/ly.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    console.log('ready to go!');
-    $('#footerDIV').load('footer.html');
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-});
